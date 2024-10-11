ffdc77a7fb21c43286beae61eb08bb6fb3f9a734
apolyeti <azhandarveen@gmail.com>
2024-10-11 08:45:59 -0700

    feat: search api works

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response } from "express";
+import { Client, PlaceType1, LatLng } from "@googlemaps/google-maps-services-js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -9,3 +12,31 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(3000, () => {
   console.log("Example app listening at http://localhost:3000");
 });
+
+app.get("/maps/:search", (req: Request, res: Response) => {
+    const API_KEY = process.env.GOOGLE_MAPS_API_KEY || "";
+    if (API_KEY === "") {
+        res.send("API_KEY is not set");
+        return;
+    }
+    
+    const location = req.params.search;
+
+    const client = new Client({});
+
+    client.textSearch({
+        params: {
+            location: "36.974117, -122.030792",
+            radius: 5000,
+            query: location,
+            type: PlaceType1.cafe,
+            key: API_KEY,
+        }
+    }).then((r) => {
+        res.send(r.data.results);
+    }
+    ).catch((e) => {
+        res.send(e);
+    });
+});
+
