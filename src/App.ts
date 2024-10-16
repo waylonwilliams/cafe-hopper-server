23c8feda80e79b0707e55604d04293c1c24f9140
apolyeti <azhandarveen@gmail.com>
2024-10-16 14:19:59 -0700

    feat: we can get place info and hours

--- src/App.ts
+++ src/App.ts
@@ -1,45 +1,41 @@
 import express, { Request, Response } from "express";
-import { TextSearch } from "../utils/maps/places/TextSearch";
+import { TextSearch, GetPlaceDetails } from "../utils/maps/Places";
+import { TextSearchResponse } from "@googlemaps/google-maps-services-js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Example app listening at http://localhost:3000");
 });
 
-app.get("/maps/:search", (req: Request, res: Response) => {
-  const API_KEY = process.env.GOOGLE_MAPS_API_KEY || "";
-  if (API_KEY === "") {
-    res.send("API_KEY is not set");
-    return;
-  }
-
+app.get("/maps/:search", async (req: Request, res: Response) => {
   const location = req.params.search;
 
   let places: any[] = [];
 
-  TextSearch(location)
-    .then((response) => {
-      response.data.results.forEach((result) => {
-        places.push({
-          name: result.name,
-          address: result.formatted_address,
-          location: result.geometry?.location,
-          rating: result.rating,
-          open: result.opening_hours?.open_now,
-          image: result.photos ? result.photos[0].photo_reference : null,
-        });
-      });
-      res.json(places);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.send("Error");
-    });
+  const textSearchResponse: TextSearchResponse = await TextSearch(location);
+
+  const results = textSearchResponse.data.results;
+
+  // We want the following fields for cafes:
+  // Title,
+  // Address,
+  // Latitude, Longitude,
+  // Hours
+
+  for (let i = 0; i < results.length; i++) {
+    const place = results[i];
+    const placeDetails = await GetPlaceDetails(place.place_id);
+    places.push({ ...placeDetails.data.result, place_id: place.place_id });
+  }
+
+  res.json(places);
 });
---
+++ utils/maps/Places.ts
@@ -0,0 +1,92 @@
+import { MapsClient } from "./Client";
+import type {
+  PlaceDetailsResponse,
+  PlacePhotoResponse,
+  TextSearchResponse,
+} from "@googlemaps/google-maps-services-js";
+import { PlaceType1 } from "@googlemaps/google-maps-services-js";
+
+/**
+ * @name TextSearch
+ * @description
+ * The Google Maps Text Search API.
+ *
+ * @example
+ * import { TextSearch } from "./utils/maps/places/TextSearch";
+ *
+ * @param query - The search query.
+ * @param geolocation - The geolocation of the user (or some predefined location).
+ */
+
+export const TextSearch = (
+  query: string,
+  geolocation?: string
+): Promise<TextSearchResponse> => {
+  const API_KEY = process.env.GOOGLE_MAPS_API_KEY || "";
+  if (API_KEY === "") {
+    throw new Error("API Key not set");
+  }
+
+  return MapsClient.textSearch({
+    params: {
+      location: geolocation || "36.974117, -122.030792", // santa cruz geolocation but we can change this later
+      radius: 5000,
+      query: query,
+      key: API_KEY,
+      type: PlaceType1.cafe,
+    },
+  });
+};
+
+// Potential ideas for grabbing info:
+// Grab the image data from these places and place them in supabase for now.
+// Grab some reviews for some data, or we can just make up some reviews.
+
+export const GetPlacePhoto = (
+  photo_reference: string
+): Promise<PlacePhotoResponse> => {
+  const API_KEY = process.env.GOOGLE_MAPS_API_KEY || "";
+  if (API_KEY === "") {
+    throw new Error("API Key not set");
+  }
+
+  return MapsClient.placePhoto({
+    params: {
+      maxheight: 100,
+      maxwidth: 100,
+      photoreference: photo_reference,
+      key: process.env.GOOGLE_MAPS_API_KEY || "",
+    },
+    responseType: "arraybuffer",
+  });
+};
+
+export const GetPlaceDetails = (
+  place_id: string | undefined
+): Promise<PlaceDetailsResponse> => {
+  if (!place_id) {
+    throw new Error("Place ID not set");
+  }
+  const API_KEY = process.env.GOOGLE_MAPS_API_KEY || "";
+  if (API_KEY === "") {
+    throw new Error("API Key not set");
+  }
+
+  return MapsClient.placeDetails({
+    params: {
+      place_id: place_id,
+      key: process.env.GOOGLE_MAPS_API_KEY || "",
+      fields: [
+        "opening_hours",
+        "name",
+        "formatted_address",
+        "geometry",
+        "icon_mask_base_uri",
+      ],
+    },
+  });
+};
+
+// export const GetPlaceHours = (placeDetails: PlaceDetailsResponse) => {
+//   return placeDetails.data.result.opening_hours;
+// };
